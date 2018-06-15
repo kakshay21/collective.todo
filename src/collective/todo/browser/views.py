920657e8d42ec509747836938ee27dd4ced06e91
Kumar Akshay <k.akshay9721@gmail.com>
2018-06-15 15:55:21 +0530

    Remove unnecessary code

--- src/collective/todo/browser/views.py
+++ src/collective/todo/browser/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
+from collective.todo import _
 from Products.Five.browser import BrowserView
 from operator import itemgetter
-from collective.todo import _
 
 
 class DemoView(BrowserView):
@@ -12,32 +12,3 @@ class DemoView(BrowserView):
 
     def for_macros(self):
         return _(u'I can be reused')
-
-    def talks(self):
-        results = []
-        data = [
-            {'title': 'Dexterity is the new default!',
-             'subjects': ('content-types', 'dexterity')},
-            {'title': 'Mosaic will be the next big thing.',
-             'subjects': ('layout', 'deco', 'views'),
-             'url': 'https://www.youtube.com/watch?v=QSNufxaYb1M'},
-            {'title': 'The State of Plone',
-             'subjects': ('keynote',)},
-            {'title': 'Diazo is a powerful tool for theming!',
-             'subjects': ('design', 'diazo', 'xslt')},
-            {'title': 'Magic templates in Plone 5',
-             'subjects': ('templates', 'TAL'),
-             'url': 'http://www.starzel.de/blog/magic-templates-in-plone-5'},
-        ]
-        for item in data:
-            try:
-                url = item['url']
-            except KeyError:
-                url = 'https://www.google.com/search?q=%s' % item['title']
-            talk = dict(
-                title=item['title'],
-                subjects=', '.join(item['subjects']),
-                url=url
-            )
-            results.append(talk)
-        return sorted(results, key=itemgetter('title'))
