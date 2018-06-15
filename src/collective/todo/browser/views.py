365cd32e3e4412bb108b13a3a6d4d023b2a77727
Kumar Akshay <k.akshay9721@gmail.com>
2018-06-15 15:12:31 +0530

    remove dynamic translation and add static one

--- src/collective/todo/browser/views.py
+++ src/collective/todo/browser/views.py
@@ -16,16 +16,16 @@ class DemoView(BrowserView):
     def talks(self):
         results = []
         data = [
-            {'title': _('Dexterity is the new default!'),
+            {'title': 'Dexterity is the new default!',
              'subjects': ('content-types', 'dexterity')},
-            {'title': _('Mosaic will be the next big thing.'),
+            {'title': 'Mosaic will be the next big thing.',
              'subjects': ('layout', 'deco', 'views'),
              'url': 'https://www.youtube.com/watch?v=QSNufxaYb1M'},
-            {'title': _('The State of Plone'),
+            {'title': 'The State of Plone',
              'subjects': ('keynote',)},
-            {'title': _('Diazo is a powerful tool for theming!'),
+            {'title': 'Diazo is a powerful tool for theming!',
              'subjects': ('design', 'diazo', 'xslt')},
-            {'title': _('Magic templates in Plone 5'),
+            {'title': 'Magic templates in Plone 5',
              'subjects': ('templates', 'TAL'),
              'url': 'http://www.starzel.de/blog/magic-templates-in-plone-5'},
         ]
